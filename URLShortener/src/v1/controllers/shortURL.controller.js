af41396411a652d042a707892cfb09f1af92927c
Christopher Montero <cristhofermontero1@gmail.com>
2022-05-12 19:30:28 -0400

    URLShort

--- URLShortener/src/v1/controllers/shortURL.controller.js
+++ URLShortener/src/v1/controllers/shortURL.controller.js
@@ -1,28 +1,55 @@
 const ShortURLs = require('../model/shortURLs');
+const dns = require('dns');
 
 const createShortURL = async (req, res) => {
   let { url } = req.body;
-  try {
-    const payload = {
-      originalUrl: url,
-    };
-    const newShortURL = new ShortURLs(payload);
+  const httpRegex = /^(http|https)(:\/\/)/;
 
-    await newShortURL.save();
-
-    const { originalUrl, shortURL } = newShortURL;
-
-    res.status(201).json({
-      status: 200,
-      data: {
-        originalUrl,
-        shortURL,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({ status: 500, error: error.message });
-    throw new Error(error.message);
+  if (!httpRegex.test(url)) {
+    return res.json({ error: 'invalid url' });
   }
+
+  const urlObject = new URL(url);
+  // verify that the domain is valid
+  dns.lookup(urlObject.hostname, async (err, address, family) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    if (!address) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    try {
+      let lastDoc = await ShortURLs.findOne().sort({ $natural: -1 });
+
+      let nextNumber;
+
+      if (!lastDoc || lastDoc.shortURL < 1) {
+        nextNumber = 1;
+      } else {
+        nextNumber = lastDoc.shortURL + 1;
+      }
+
+      const payload = {
+        originalUrl: url,
+        shortURL: nextNumber,
+      };
+      const newShortURL = new ShortURLs(payload);
+
+      await newShortURL.save();
+
+      const { originalUrl, shortURL } = newShortURL;
+
+      res.status(201).json({
+        original_url: originalUrl,
+        short_url: shortURL,
+      });
+    } catch (error) {
+      res.status(500).json({ status: 500, error: error.message });
+      throw new Error(error.message);
+    }
+  });
 };
 
 const redirectToShortURL = async (req, res) => {
